30b71ea0f7c9832245ba66d58f34678c0a2a43c3
ErickDiaz21 <erickomar_@hotmail.com>
2015-10-27 15:34:46 -0600

    Version1.0

---
+++ clase1/app/src/main/java/erickdiazclase1/clase1/MainActivity.java
@@ -0,0 +1,14 @@
+package erickdiazclase1.clase1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Edi
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
